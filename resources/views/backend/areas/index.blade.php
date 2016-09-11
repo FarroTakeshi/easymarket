c8601ca2655f446440073b6437a3905dee21b15e
takeshi <takeshi.farro@gmail.com>
2016-09-11 01:23:27 -0500

    model and controller, blades of area and sb admin 2 added

---
+++ resources/views/backend/areas/index.blade.php
@@ -0,0 +1,56 @@
+@extends('backend.layout.base')
+
+@section('content')
+
+@if( session()->has('message') )
+<div class="row">
+   <div class="alert alert-success">
+      {{ session('message') }}
+   </div>
+</div>
+@endif
+
+@if( session('sad_message') )
+<div class="row">
+   <div class="alert alert-danger">
+      {{session('sad_message') }}
+   </div>
+</div>
+@endif
+
+<div class="row">
+   <div class="row">
+   <div class="col-xs-12">
+      <h2>Areas</h2>
+   </div>
+</div>
+
+   <table class="table">
+      <thead>
+         <th>Name</th>
+         <th>Description</th>
+         <th>Actions</th>
+      </thead>
+      <tbody>
+         @foreach($areas as $area)
+         <tr>
+            <td>{{ $area->name }}</td>
+            <td>{{ $area->description }}</td>
+            <td>
+               <a class="btn btn-default" href="{{ route('areas.edit', ['id' => $area->id]) }}">Edit</a>
+               <a class="btn btn-danger" id="delete_button" href="{{ route('areas.delete', ['id' => $area->id]) }}" onclick="return confirm('Are you sure you want to delete this item?');">Delete</a>
+            </td>
+         </tr>
+         @endforeach
+      </tbody>
+   </table>
+
+</div>
+
+<div class="row">
+   <div class="form-group">
+      <a href="{{ route('areas.create', ['area' => $area->id ]) }}" class="btn btn-success">Add</a>
+   </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
+Route::group(['middleware' => 'web'], function () {
+   //Area
+    Route::group(['prefix' => 'areas', 'as' => 'areas.'], function(){
+      Route::get('/index', ['as' => 'index', 'uses' => 'AreaController@index']);
+      Route::get('/new', ['as' => 'create', 'uses' => 'AreaController@create']);
+      Route::post('/new', ['as' => 'store', 'uses' => 'AreaController@store']);
+      Route::get('/edit/{id}', ['as' => 'edit', 'uses' => 'AreaController@edit']);
+      Route::post('/edit/{id}', ['as' => 'update', 'uses' => 'AreaController@update']);
+      Route::get('/delete/{id}', ['as' => 'delete', 'uses' => 'AreaController@delete']);
+   });
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
